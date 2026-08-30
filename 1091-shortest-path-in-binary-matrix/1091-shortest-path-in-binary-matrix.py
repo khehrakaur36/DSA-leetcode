d895bd653c8972c5720079ac20e6d04942cbdd10
khehrakaur36 <kirankhehra62@gmail.com>
2026-08-30 20:09:13 +0530

    Time: 124 ms (80.62%), Space: 19.8 MB (60.93%) - LeetHub

---
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -0,0 +1,31 @@
+class Solution:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+
+        if grid[0][0] ==1 or grid[n-1][n-1]==1:
+            return -1
+
+        queue = deque()
+        queue.append((0,0,1))
+
+        grid[0][0] =1 
+        while queue:
+            row , col , dist = queue.popleft()
+
+            if row == n-1 and col == n-1 :
+                return dist
+
+            directions = [
+                (-1,-1) , (-1,0), (-1, 1),
+                (0,-1) ,          (0,1),
+                (1, -1) , (1,0) , (1,1)
+            ]        
+
+            for dr , dc in directions:
+                nr = row + dr
+                nc = col + dc
+
+                if 0<= nr <n and 0<= nc <n and grid[nr][nc] == 0:
+                    grid[nr][nc] =1
+                    queue.append((nr, nc, dist+1))
+        return -1            
